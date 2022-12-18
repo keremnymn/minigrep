2d3f7400175d179d832ed4a47a8d7a282fe80d09
Kerem Nayman <keremnymn@gmail.com>
2022-12-18 22:00:14 +0100

    case sensitive input

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Error while parsing the arguments: {}", err);
+        eprintln!("Error while parsing the arguments: {}", err);
         process::exit(1)
     });
 
-    println!("Searching for {}\nIn file {}", config.query, config.file_path);
+    println!("Searching for {}\nIn file {}...\n", config.query, config.file_path);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1)
     }
 }
